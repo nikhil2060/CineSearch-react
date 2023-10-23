7a56cb6aa5567f3a846e7e564511a334fbb622d8
nikhil2060 <kumarnikhil2060@gmail.com>
2023-10-23 22:25:40 +0530

    fetch url change

--- src/App -v2.js
+++ src/App -v2.js
@@ -242,7 +242,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
         try {
           setIsLoading(true);
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+            `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
           );
 
           if (!res.ok) throw new Error("Cant able to fetch");
